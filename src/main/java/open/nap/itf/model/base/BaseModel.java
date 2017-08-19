104b786076cb2d6e7bd45313f4430fd9d210318c
wreckbear <wreckbear@163.com>
2017-08-19 14:24:04 +0800

    change

--- src/main/java/open/nap/itf/model/base/BaseModel.java
+++ src/main/java/open/nap/itf/model/base/BaseModel.java
@@ -1,24 +1,18 @@
 package open.nap.itf.model.base;
 
-public interface BaseModel {
+public abstract class BaseModel {
 
 	/**
 	 * 得到该实体所对应的表名
 	 * @return
 	 */
-	public String getTableName();
+	public abstract String getTableName();
 	
-	
-	/**
-	 * 得到该实体的全类名
-	 * @return
-	 */
-	public String getModelClassName();
 	
 	/**
 	 * 得到该实体的主键名
 	 * @return
 	 */
-	public String getModelIDName();
+	public abstract String getModelIDName();
 	
 }
